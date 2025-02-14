dd5fcd4158dc26696f61b85b49fcea5e12af1187
crapbass <64779013+expected-ingot@users.noreply.github.com>
2025-02-14 19:13:31 +0100

    Add a few functions to MemorySys.c (#27)

--- src/game/MemorySys.c
+++ src/game/MemorySys.c
@@ -20,7 +20,16 @@ INCLUDE_ASM("asm/game/nonmatchings/MemorySys", MemorySys__Init01);
 
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", MemorySys__malloc);
 
+#ifndef NON_MATCHING
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", MemorySys__free);
+#else
+void MemorySys__free(s32 address) {
+    if (address != 0) {
+        func_8003439C();
+        freeAmount += 1;
+    }
+}
+#endif
 
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", MemorySys__CountHeapFree);
 
@@ -118,21 +127,70 @@ INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_800231C4);
 
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_800231F4);
 
-INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80023210);
+s32 func_80023210(s32 value) {
+    // rounds up a value to the nearest multiple of 4
+    return (value + 3) & ~3;
+}
 
+#ifndef NON_MATCHING
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80023220);
+#else
+s32 func_80023220(s8* first_string, s8* second_string) {
+    // https://decomp.me/scratch/MVnJF
+    s8 current_character = *first_string;
+    s8 other_character = *second_string;
 
-INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_800232A0);
+    // uppercase to lowercase (A-Z)
+    if ((u32)(current_character - 65) < 26) {
+        current_character += 0x20;
+    }
+    if ((u32)(other_character - 65) < 26) {
+        other_character += 0x20;
+    }
+
+    // compare characters?
+    if (current_character < other_character) {
+        return -1;
+    }
+    if (other_character < current_character) {
+        return 1;
+    }
+    
+    if (current_character != 0) {
+        return func_80023220(first_string + 1, second_string + 1);
+    }
+    
+    return 0;
+}
+#endif
+
+s32 func_800232A0(s32 arg0) {
+    return arg0 + func_80023210(func_80035C6C() + 1);
+}
 
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_800232D0);
 
-INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80023388);
+s32 func_80023388(void) {
+    s32 temp_return_value;
+
+    temp_return_value = func_800232D0();
+
+    if (temp_return_value != 0) {
+        return func_800232A0(temp_return_value); 
+    }
+
+    return 0;
+}
 
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", main);
 
-INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80023A7C);
+s32 func_80023A7C(s32 arg0, s32 arg1, s32 arg2) {
+    return arg0 + (arg2 - arg1);
+}
 
-INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80023A88);
+s32 func_80023A88(s32 arg0, s32 arg1, s32 arg2) {
+    return arg0 + (arg2 - arg1);
+}
 
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80023A94);
 
@@ -140,9 +198,15 @@ void func_80023AF0(void) {}
 
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80023AF8);
 
-INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80023B08);
+s32* func_80023B08(s32* arg0) {
+    *arg0 = 0;
+    return arg0;
+}
 
-INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80023B14);
+s32* func_80023B14(s32* arg0) {
+    *arg0 = 0;
+    return arg0;
+}
 
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80023B20);
 
@@ -154,7 +218,24 @@ INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_800245CC);
 
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_800246EC);
 
+#ifndef NON_MATCHING
 INCLUDE_ASM("asm/game/nonmatchings/MemorySys", func_80024820);
+#else
+extern int (*D_80048028)();
+
+void func_80024820(s32 arg0) {
+    int (*var_v0)();
+
+    do {
+        var_v0 = D_80048028;
+        if (var_v0 == NULL) {
+            printf("out of memory\n");
+            exit(1);
+        }
+        var_v0();
+    } while (MemorySys__malloc(arg0) == 0);
+}
+#endif
 
 void func_8002487C(void) {}
 
